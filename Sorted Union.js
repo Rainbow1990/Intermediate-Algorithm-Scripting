a4fcdfef2299ceb74979e34bf7c1e92c762be041
Rainbow1990 <Rainbow10222@gmail.com>
2022-12-30 17:17:47 +0300

    Intermediate Algorithm Scripting

---
+++ Sorted Union.js
@@ -0,0 +1,16 @@
+function uniteUnique(arr) {
+    let finalCollection = [];
+    let values = Object.values(arguments);
+  
+    values.forEach(groupOfGroups => {
+      groupOfGroups.forEach(individualValues => {
+        if (finalCollection.indexOf(individualValues) === -1) {
+          finalCollection.push(individualValues);
+        }
+      });
+    });
+    return finalCollection;
+  }
+  
+  let result = uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+  console.log(result);
